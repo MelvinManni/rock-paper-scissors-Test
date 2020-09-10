c27d20749dbe44a6f8b66bb9183a45a24a867fa6
Miracle Gabriel <sixpathdev@gmailc.com>
2020-09-10 18:44:36 +0100

    BonusPlay.js and SelectPlay.js refactored

--- src/Selectplay.js
+++ src/Selectplay.js
@@ -3,7 +3,10 @@ import App from "./App";
 import BonusPlay from "./BonusPlay";
 
 const Selectplay = () => {
+
 const weapons = ["rock", "paper", "scissors"];
+const bonusweapons = ["rock", "paper", "scissors", "lizard", "spock"];
+
   const [mode, setMode] = useState("original");
   const [playerOne, setPlayerOne] = useState(weapons[0]);
   const [playerTwo, setPlayerTwo] = useState(weapons[0]);
@@ -25,7 +28,41 @@ const weapons = ["rock", "paper", "scissors"];
     }, 100);
   };
 
+  const startBonusGame = () => {
+    let counter = 0;
+    let gameInterval = setInterval(() => {
+      counter++;
+      setPlayerTwo(bonusweapons[Math.floor(Math.random() * bonusweapons.length)])
+      setWinner("")
+      if (counter > 9) {
+        clearInterval(gameInterval);
+        setWinner(selectBonusWinner)
+      }
+    }, 100);
+  };
+
   const selectWinner = () => {
+    if (playerOne === playerTwo) {
+      return "It's a Tie!";
+    } else if (
+      (playerOne === "rock" && playerTwo === "scissors") ||
+      (playerOne === "rock" && playerTwo === "lizard") ||
+      (playerOne === "paper" && playerTwo === "rock") ||
+      (playerOne === "paper" && playerTwo === "spock") ||
+      (playerOne === "scissors" && playerTwo === "paper") ||
+      (playerOne === "scissors" && playerTwo === "lizard") ||
+      (playerOne === "spock" && playerTwo === "scissors") ||
+      (playerOne === "spock" && playerTwo === "rock") ||
+      (playerOne === "lizard" && playerTwo === "paper") ||
+      (playerOne === "lizard" && playerTwo === "spock")
+    ) {
+      return "Player One Wins!";
+    } else {
+      return "Player Two Wins!";
+    }
+  };
+
+  const selectBonusWinner = () => {
     if (playerOne === playerTwo) {
       return "It's a Tie!";
     } else if (
@@ -62,7 +99,14 @@ const weapons = ["rock", "paper", "scissors"];
       selectWeapon={selectWeapon}
     />
   ) : (
-    <BonusPlay updateCom={updateCom} />
+    <BonusPlay 
+    updateCom={updateCom}
+    startBonusGame={startBonusGame}
+    playerOne={playerOne}
+    playerTwo={playerTwo}
+    winner={winner}
+    selectBonusWinner={selectBonusWinner}
+    selectWeapon={selectWeapon} />
   );
 };
 
